b4640d9ebd05c061e82eadc9c0948c98ec3bf6bc
graham <grahamdrichards@gmail.com>
2018-07-10 05:59:44 +0100

    Added do2 that gives a promise interface

--- get_open_jobs.js
+++ get_open_jobs.js
@@ -3,9 +3,9 @@
 let jobtracking = require('./jobtracking_common.js')();
 
 jobtracking.init().then( function( { config, logger, model } ) {
-  model.do( 'listjobs', { state: 'OPEN' }, function( modelres ) {
-    logger.info( modelres );
-  });
-
+  model.do2( 'listjobs', { state: 'OPEN' } )
+    .then ( function( modelres ) {
+      logger.info( modelres );
+    });
 });
 
